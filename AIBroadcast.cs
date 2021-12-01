84ac0c2b9e55537c5e14452e882318a371654a7a
302 <302.com>
2021-12-01 17:57:40 +0800

    AI

---
+++ AIBroadcast.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//这个类要单例
+public class AIBroadcast : MonoBehaviour {
+    public bool noonTime = false;
+    private void Update()
+    {
+        if (true/*休息时间*/)
+        {
+            noonTime = true;
+        }
+    }
+
+}
