4a4e5faade80e8115126466f62ba26b9dad72e3b
302 <302.com>
2021-12-01 16:03:19 +0800

    NPCAI

---
+++ NPCTry_302.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public enum NPCKinds
+{
+    MERCHANT,
+    FARMER,
+    NOMAD
+}
+public class NPCTry_302 : MonoBehaviour {
+    NavMeshAgent agent;
+    Animator ani;
+    public Transform homePos;
+    public int wakeUpTime;
+    public Vector3 drinkPos;
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        ani = GetComponent<Animator>();
+    }
+    private void ToDrink()
+    {
+        agent.destination = drinkPos;
+        //开喝
+    }
+
+    private void FreeTime()
+    {
+        int i;
+        if (true/*如果到了他起床的点*/)
+        {
+            //切换动作
+            i = Random.Range(0,10);
+        }+
+        switch (i)
+        {
+            case 1:
+                ToDrink();
+                break;
+        }
+    }
+
+}
