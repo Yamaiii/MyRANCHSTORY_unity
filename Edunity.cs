87bb53537c78b544f959d523aa5703d2bc0a2b8d
307 <307.com>
2021-12-03 11:49:45 +0800

    307

---
+++ Edunity.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Edunity : Editor {
+    [MenuItem("log/打开更新")]
+    public static void OpenFilePath()
+    {
+        string path = Application.persistentDataPath;
+        if (!System.IO.Directory.Exists(path))
+        {
+            System.IO.Directory.CreateDirectory(path);
+        }
+        EditorUtility.RevealInFinder(path);
+    }
+}
---
+++ WebReque.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class WebReque : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public IEnumerator LoadTextAssets()
+    {
+        System.Uri uri = new System.Uri(System.IO.Path.Combine(Application.streamingAssetsPath,"config,json"));
+        UnityWebRequest unityWebRequest = UnityWebRequest.Get(uri.AbsoluteUri);
+        yield return unityWebRequest.SendWebRequest();
+    }
+}
